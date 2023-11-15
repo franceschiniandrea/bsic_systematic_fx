3ecd9f71f40d390c0fc4c195cd72bbdb8fd1a096
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-15 18:08:40 +0100

    init function

--- src/backtester.py
+++ src/backtester.py
@@ -1,6 +1,23 @@
+import pandas as pd
+
+# TODO implement slippage and transaction costs
+
+
 class Backtest:
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        fx_lon_fixes: pd.DataFrame,
+        fx_ny_fixes: pd.DataFrame,
+        swaps_lon_fixes: pd.DataFrame,
+        swaps_ny_fixes: pd.DataFrame,
+        ma_window: int = 15,
+    ) -> None:
+        self.fx_lon_fixes = fx_lon_fixes
+        self.fx_ny_fixes = fx_ny_fixes
+        self.swaps_lon_fixes = swaps_lon_fixes
+        self.swaps_ny_fixes = swaps_ny_fixes
+
+        self.ma_window = ma_window
 
     def signal(self):
         pass
