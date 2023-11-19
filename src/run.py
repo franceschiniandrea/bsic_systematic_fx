56c056e06e6449c9e1cf7ad3684c5904eb09fbbb
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-19 22:11:41 +0100

    feat: debug code

--- src/run.py
+++ src/run.py
@@ -3,18 +3,22 @@ from typing import Literal
 
 import pandas as pd
 
-from backtester import Backtest, Fixes
+from backtester import Backtest
 from load_data import load_data
 
-fx_lon_fix, fx_ny_fix, swaps_lon_fix, swaps_ny_fix = load_data()
-backtest = Backtest(
-    fx_lon_fix, fx_ny_fix, swaps_lon_fix, swaps_ny_fix, ma_window=15, logging_level=1
-)
+fx_fixes, swaps_fixes = load_data()
+backtest = Backtest(fx_fixes, swaps_fixes, ma_window=3, logging_level=1)
 
+print(fx_fixes, fx_fixes.index)
+
+backtest.run()
 # backtest.run(fx_lon_fix.index[-2], fx_lon_fix.index[0])
 # backtest.run(fx_lon_fix.index[-2], fx_lon_fix.index[0], rebalancing_freq="W-MON")
-
-backtest.compute_signals(fix=Fixes.LON)
-backtest.compute_positions(fix=Fixes.LON)
-backtest.compute_pnl()
+# backtest.compute_signals(fix=Fixes.NY)
+# backtest.compute_positions(fix=Fixes.LON)
+# backtest.compute_positions(fix=Fixes.NY)
+# backtest.compute_pnl()
 backtest.compute_stats()
+
+
+s = backtest.signal
