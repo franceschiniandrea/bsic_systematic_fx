19d71cd702b03b09efe7c2102311e9a4069c089f
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-19 03:08:43 +0100

    fix: modified with new functions from backtester

--- src/run.py
+++ src/run.py
@@ -1,9 +1,20 @@
-from backtester import Backtest
+import logging
+from typing import Literal
+
+import pandas as pd
+
+from backtester import Backtest, Fixes
 from load_data import load_data
 
 fx_lon_fix, fx_ny_fix, swaps_lon_fix, swaps_ny_fix = load_data()
-backtest = Backtest(fx_lon_fix, fx_ny_fix, swaps_lon_fix, swaps_ny_fix, ma_window=15)
+backtest = Backtest(
+    fx_lon_fix, fx_ny_fix, swaps_lon_fix, swaps_ny_fix, ma_window=15, logging_level=1
+)
 
-print(swaps_lon_fix)
+# backtest.run(fx_lon_fix.index[-2], fx_lon_fix.index[0])
+# backtest.run(fx_lon_fix.index[-2], fx_lon_fix.index[0], rebalancing_freq="W-MON")
 
-backtest.test("10-10-2017")
+backtest.compute_signals(fix=Fixes.LON)
+backtest.compute_positions(fix=Fixes.LON)
+backtest.compute_pnl()
+backtest.compute_stats()
