5a0863f294a1b22ff659edda6d58315594da0c07
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-25 02:53:50 +0100

    feat: robustness checks on paper and improved strategy

---
+++ src/robcheck.py
@@ -0,0 +1,40 @@
+import logging
+from itertools import product
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from backtester.strategy_improved import ImprovedStrategy as ImprStratSigm
+from load_data import load_data
+
+log = logging.getLogger("backtester")
+log.setLevel(30)
+
+fx_fixes, swaps_fixes, cpi_data, real_swaps_fixes = load_data()
+swaps_fixes.ffill(inplace=True)
+fx_fixes.ffill(inplace=True)
+
+BT_FX = fx_fixes
+BT_SWAPS = swaps_fixes
+
+MA_WINDOW_LONGS = range(10, 31, 1)
+MA_WINDOW_SHORTS = range(2, 11)
+REBAL_THRESHOLD = 3
+
+sharpes = pd.DataFrame(index=MA_WINDOW_SHORTS, columns=MA_WINDOW_LONGS)
+
+for short, long in product(MA_WINDOW_SHORTS, MA_WINDOW_LONGS):
+    print(f"{short}, {long}")
+    MA_WINDOW = (short, long)
+
+    bt = ImprStratSigm(fx_fixes, swaps_fixes, short, long, REBAL_THRESHOLD, None)
+
+    bt.run()
+
+    perf = bt.compute_stats()
+
+    sharpes.loc[short, long] = perf.loc["average", "sharpe"]
+
+print(sharpes)
+
+sharpes.to_excel("results/improved_tot.xlsx")
---
+++ src/robcheck_paper.py
@@ -0,0 +1,38 @@
+import logging
+from itertools import product
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from backtester.strategy_paper import PaperStrategy
+from load_data import load_data
+
+log = logging.getLogger("backtester")
+log.setLevel(30)
+
+fx_fixes, swaps_fixes, cpi_data, real_swaps_fixes = load_data()
+swaps_fixes.ffill(inplace=True)
+fx_fixes.ffill(inplace=True)
+
+BT_FX = fx_fixes
+BT_SWAPS = swaps_fixes
+
+MA_WINDOWS = range(2, 31)
+REBAL_THRESHOLD = 3
+
+sharpes = pd.DataFrame(index=MA_WINDOWS, columns=["sharpe"])
+
+for window in MA_WINDOWS:
+    print(f"{window}")
+
+    bt = PaperStrategy(fx_fixes, swaps_fixes, window, REBAL_THRESHOLD, None)
+
+    bt.run()
+
+    perf = bt.compute_stats()
+
+    sharpes.loc[window, "sharpe"] = perf.loc["average", "sharpe"]
+
+print(sharpes)
+
+sharpes.to_excel("results/paper_tot.xlsx")
